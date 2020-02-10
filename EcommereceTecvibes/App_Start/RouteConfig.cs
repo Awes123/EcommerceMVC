60583429cc67a9fc76efb9fac45f93a2da527962
Awes Bagban <53370147+Awes123@users.noreply.github.com>
2020-02-11 00:38:56 +0530

    Design Theme, Load Category Load Products

--- EcommereceTecvibes/App_Start/RouteConfig.cs
+++ EcommereceTecvibes/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace EcommereceTecvibes
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Login", action = "Login", id = UrlParameter.Optional }
+                defaults: new { controller = "Shop", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
---
+++ EcommereceTecvibes/Controllers/ShopController.cs
@@ -0,0 +1,23 @@
+﻿using EcommereceTecvibes.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EcommereceTecvibes.Controllers
+{
+    public class ShopController : Controller
+    {
+        EcommerceEntities db = new EcommerceEntities();
+        // GET: Default
+        public ActionResult Index()
+        {
+            List<Category> category = db.Categories.ToList();
+            ViewBag.Category = category;
+            var firstcatid = category.Select(s => s.Category_ID).FirstOrDefault();
+            ViewBag.product = db.Products.Where(e=>e.Category_ID== firstcatid).ToList();
+            return View();
+        }
+    }
+}
